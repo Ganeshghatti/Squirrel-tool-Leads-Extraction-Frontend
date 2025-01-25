34a5123c766be96254dfe54681461bae00e59b69
aasuyadav1 <aasuy284@gmail.com>
2025-01-25 18:42:13 +0530

    fix: error

--- src/app/dashboard/leads/[id]/page.tsx
+++ src/app/dashboard/leads/[id]/page.tsx
@@ -1,15 +1,12 @@
 import LeadCollectionContainer from "@/components/dashboard/leads/LeadsContainer";
+import { use } from "react";
 
-export default async function Page({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const { id } = await params
+export default function Page({ params }: { params: Promise<{ id: string }> }) {
+  const resolvedParams = use(params);
   return (
     <div className="p-4 w-full">
       <h1 className="text-2xl font-bold mb-4">Lead Collections</h1>
-      <LeadCollectionContainer id={id} />
+      <LeadCollectionContainer id={resolvedParams.id} />
     </div>
   );
 }
